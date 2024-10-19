cdc5b9ff44029317795a47ef201300aacdb8c2bc
roy wahome <roy.njuguna@student.moringaschool.com>
2024-10-19 18:39:08 +0300

    fixing another error

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # Import Migrate
-from flask_cors import CORS
+from flask_migrate import Migrate
 
-# Initialize extensions
 db = SQLAlchemy()
-migrate = Migrate()  # Define Migrate instance
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config.Config')  # Assuming you have a config.py file
 
-    # Configuration
-    app.config.from_object('config.Config')
-
-    # Initialize extensions
     db.init_app(app)
-    migrate.init_app(app, db)  # Initialize Migrate with app and db
-    CORS(app)
+    migrate.init_app(app, db)
 
-    # Import and register blueprints
-    from .routes import main as main_blueprint
+    # Import and register your blueprints
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
